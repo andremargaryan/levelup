fe824a8349166c2777b7e6fd12325542d94963dd
steve-y68 <ysteve322@gmail.com>
2026-03-30 18:18:29 +0200

    Ajout du Mini-sudoku

---
+++ src/Controller/Sudoku.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class Sudoku extends AbstractController
+{
+    #[Route('/jeux/sudoku', name: 'app_sudoku')]
+    public function sudoku(): Response
+    {
+        // On définit la grille, mais on ne lance AUCUNE fonction de résolution ici
+        $grille = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ];
+
+        // On envoie juste les données brutes au template
+        return $this->render('jeux/sudoku.html.twig', [
+            'grille' => $grille
+        ]);
+    }
+}
